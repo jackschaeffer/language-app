ec3ec88bddc1fc849c56105c6e13d8a834104c54
Jack Schaeffer <jackschaeffer@outlook.com>
2021-10-04 12:24:55 -0500

    Initial commit

---
+++ backend/spring-boot-language-app/src/main/java/com/jackschaeffer/languageapp/SpringBootLanguageAppApplication.java
@@ -0,0 +1,13 @@
+package com.jackschaeffer.languageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLanguageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLanguageAppApplication.class, args);
+	}
+
+}
---
+++ backend/spring-boot-language-app/src/test/java/com/jackschaeffer/languageapp/SpringBootLanguageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackschaeffer.languageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLanguageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
