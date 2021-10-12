a8825dc5a86a8b1a52cc3b6e7d1e40430018981e
Jack Schaeffer <jackschaeffer@outlook.com>
2021-10-12 10:28:07 -0500

    Define angular routes and configure router

--- frontend/angular-languageapp/src/app/app.module.ts
+++ frontend/angular-languageapp/src/app/app.module.ts
@@ -5,6 +5,16 @@ import { AppComponent } from './app.component';
 import { SongListComponent } from './components/song-list/song-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SongService } from './services/song.service';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {path: 'genres/:id', component: SongListComponent},
+  {path: 'genre', component: SongListComponent},
+  {path: 'songs', component: SongListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -12,6 +22,7 @@ import { SongService } from './services/song.service';
     SongListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
