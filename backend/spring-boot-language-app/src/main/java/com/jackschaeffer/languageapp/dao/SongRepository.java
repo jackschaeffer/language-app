c85366b8246b9f43a84abc17df37797faad326c2
Jack Schaeffer <jackschaeffer@outlook.com>
2021-10-14 12:15:13 -0500

    Add support for spring query song by artist and genre ids

--- backend/spring-boot-language-app/src/main/java/com/jackschaeffer/languageapp/dao/SongRepository.java
+++ backend/spring-boot-language-app/src/main/java/com/jackschaeffer/languageapp/dao/SongRepository.java
@@ -1,9 +1,17 @@
 package com.jackschaeffer.languageapp.dao;
 
 import com.jackschaeffer.languageapp.entity.Song;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @CrossOrigin("http://localhost:4200")
 public interface SongRepository extends JpaRepository<Song, Long> {
+
+    Page<Song> findByGenreId(@RequestParam("id") Long id, Pageable pageable);
+
+    Page<Song> findByArtistId(@RequestParam("id") Long id, Pageable pageable);
 }
